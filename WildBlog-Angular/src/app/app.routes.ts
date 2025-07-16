2401848fdc3411519484d6c72d368911c5a32e27
Claire Cellier <ccellier@aubay.com>
2025-07-16 14:46:27 +0200

    Angular-Les templates driven forms

--- WildBlog-Angular/src/app/app.routes.ts
+++ WildBlog-Angular/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ArticlePageComponent } from './pages/article-page/article-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent, pathMatch: 'full' },
     { path: 'article/:id', component: ArticlePageComponent },
+    { path: 'contact', component: ContactPageComponent},
     { path: '**', component: NotFoundComponent}
 ];
